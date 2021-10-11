352684d1ff4714cb5026122b2b12cf368e8bb9a9
yoshikifujiwara <fujiwara@offgrid.co.jp>
2021-10-11 16:43:51 +0900

    修正

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ class TestCICD:
         try:
             # Chromeを操作
             options = Options()
-            # options.add_argument('--headless')
+            options.add_argument('--headless')
             driver = webdriver.Chrome(options=options)
             url = "http://127.0.0.1:5000/demo"
             driver.get(url)
